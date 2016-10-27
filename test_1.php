497588ff5f69ec2df668cd38a06bbd723307d092
Nikolaj Stockholm <nst@efiware.com>
2016-10-27 08:51:42 +0200

    Import

---
+++ test_1.php
@@ -0,0 +1,13 @@
+<?php 
+
+/**
+ * Test #1
+ * 
+ * Retrieve the answer, follow the leads along.
+ * It is expected to code the whole solution below, and make it print the answer.
+ * 
+ * @file test_1.txt
+ */
+
+
+#e2f64796272757260237960227567737e6160256864502e256d69647023796864702e696770257f69502e24757364727f686370256864702b6f6f6470257f695
